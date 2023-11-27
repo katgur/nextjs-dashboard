6efc6e47d0fda60f388b81bbcb6e3731c9158d7a
Kate <mitsutanaka29@gmail.com>
2023-11-27 19:22:04 +0300

    Chapter 5: Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
             width={560}
             height={620}
             className="block md:hidden"
-            alt="Screenshots of the dashboard project showing desktop version"
+            alt="Screenshots of the dashboard project showing mobile version"
           />
           <Image
             src="/hero-desktop.png"
